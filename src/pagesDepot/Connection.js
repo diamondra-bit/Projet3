d9cb9de2d10bad4e93744f2f36ee5bb5e48c18b5
Finoana7 <finoana.marinah@gmail.com>
2023-10-03 02:35:33 +0300

    03-sept

--- src/pagesDepot/Connection.js
+++ src/pagesDepot/Connection.js
@@ -6,47 +6,48 @@ import '../pagesDepot/cssDepot/connection.css'
 function Connection() {
 
   return (
+    
     <>
-      <div>Connection</div>
+      <div className="body">
 
-        <div class="container">
-          <div class="box">
-            <div class="content">
-                <h2>01</h2>
-                <h3>Employé</h3>
-                <p>Petite description...</p>
-                <Link to={"/LoginEmploye"}>
-                  <button>Employé</button>
-                </Link>
+        <div className="container">
+
+            <div className="box">
+              <div className="content">
+                  <h2>01</h2>
+                  <h3>Employé</h3>
+                  <p>Petite description...</p>
+
+                  <Link to={"/LoginEmploye"}>
+                    <button>Employé</button>
+                  </Link>
+              </div>
+            </div>  
+
+            <div className="box">
+              <div className="content">
+                  <h2>02</h2>
+                  <h3>Dépôt</h3>
+                  <p>Petite description...</p>
+                  <Link to={"/LoginDepot"}>
+                    <button>Dépot</button>
+                  </Link>
+              </div>
+            </div>  
+
+            <div className="box">
+              <div className="content">
+                  <h2>03</h2>
+                  <h3>Sécurité</h3>
+                  <p>Petite description...</p>
+                  <Link to={"/LoginSecurite"}>
+                    <button>Sécurité</button>
+                  </Link>
+              </div>
             </div>
-          </div>  
+            
         </div>
-
-        <div class="container">
-          <div class="box">
-            <div class="content">
-                <h2>02</h2>
-                <h3>Dépôt</h3>
-                <p>Petite description...</p>
-                <Link to={"/LoginDepot"}>
-                  <button>Dépot</button>
-                </Link>
-            </div>
-          </div>  
-        </div>
-
-        <div class="container">
-          <div class="box">
-            <div class="content">
-                <h2>03</h2>
-                <h3>Sécurité</h3>
-                <p>Petite description...</p>
-                <Link to={"/LoginSecurite"}>
-                  <button>Sécurité</button>
-                </Link>
-            </div>
-          </div>  
-        </div>      
+      </div>      
     </>
 
   )
