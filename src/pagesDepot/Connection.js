6246f8943e44e28ee2111a6d21e8456cf48287f1
Finoana7 <finoana.marinah@gmail.com>
2023-10-04 19:04:38 +0300

    04/10

--- src/pagesDepot/Connection.js
+++ src/pagesDepot/Connection.js
@@ -7,21 +7,19 @@ function Connection() {
 
   return (
     <>
-      <div className='bodyConnection'>
+      <div className="bodyConnection">
         <div class="containerConnection">
-          <div class="box">
-            <div class="contentConnection">
-                <h2 class="numero">01</h2>
-                <h3 class="titre">Employé</h3>
-                <p class="paragrapheConnection">Petite description...</p>
-                <Link to={"/LoginEmploye"} class="btnConnection">
-                  <button >Entrer</button>
-                </Link>
-            </div>
-          </div>  
-        
+            <div class="box">
+              <div class="contentConnection">
+                  <h2 class="numero">01</h2>
+                  <h3 class="titre">Employé</h3>
+                  <p class="paragrapheConnection">Petite description...</p>
+                  <Link to={"/LoginEmploye"} class="btnConnection">
+                    <button >Entrer</button>
+                  </Link>
+              </div>
+            </div>  
 
-          <div class="containerConnection">
             <div class="box">
               <div class="contentConnection">
                   <h2 class="numero">02</h2>
@@ -31,10 +29,8 @@ function Connection() {
                     <button>Entrer</button>
                   </Link>
               </div>
-            </div>
-          </div>   
+            </div> 
         
-          <div class="containerConnection">
             <div class="box">
               <div class="contentConnection">
                   <h2 class="numero">03</h2>
@@ -45,7 +41,6 @@ function Connection() {
                   </Link>
               </div>
             </div>
-          </div>
         </div>
       </div>
     </>
