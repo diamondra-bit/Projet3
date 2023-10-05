5f17e0bc19f2566cdb91ddaf52a8e41ea3950e35
Finoana7 <finoana.marinah@gmail.com>
2023-10-05 18:02:44 +0300

    connection

--- src/pagesDepot/Connection.js
+++ src/pagesDepot/Connection.js
@@ -5,40 +5,38 @@ import '../pagesDepot/cssDepot/connection.css'
 
 function Connection() {
 
+  const styleSansBordure = {
+    textDecoration: 'none', // Pour supprimer la bordure de l'élément Link
+  };
+
   return (
     <>
       <div className="bodyConnection">
-        <div class="containerConnection">
-            <div class="box">
-              <div class="contentConnection">
-                  <h2 class="numero">01</h2>
-                  <h3 class="titre">Employé</h3>
-                  <p class="paragrapheConnection">Petite description...</p>
-                  <Link to={"/LoginEmploye"} class="btnConnection">
-                    <button >Entrer</button>
-                  </Link>
+        <div className="containerConnection">
+            <div className="box">
+              <div className="contentConnection">
+                  <h2 className="numero">01</h2>
+                  <h3 className="titre">Employé</h3>
+                  <p className="paragrapheConnection">Petite description...</p>
+                  <Link to={"/LoginEmploye"} className="btnConnection" style={styleSansBordure}>Entrer</Link>
               </div>
             </div>  
 
-            <div class="box">
-              <div class="contentConnection">
-                  <h2 class="numero">02</h2>
-                  <h3 class="titre">Dépôt</h3>
+            <div className="box">
+              <div className="contentConnection">
+                  <h2 className="numero">02</h2>
+                  <h3 className="titre">Dépôt</h3>
                   <p class="paragrapheConnection">Petite description...</p>
-                  <Link to={"/LoginDepot"}  class="btnConnection">
-                    <button>Entrer</button>
-                  </Link>
+                  <Link to={"/LoginDepot"}  className="btnConnection">Entrer</Link>
               </div>
             </div> 
         
-            <div class="box">
-              <div class="contentConnection">
-                  <h2 class="numero">03</h2>
-                  <h3 class="titre">Sécurité</h3>
-                  <p class="paragrapheConnection">Petite description...</p>
-                  <Link to={"/LoginSecurite"}  class="btnConnection">
-                    <button>Entrer</button>
-                  </Link>
+            <div className="box">
+              <div className="contentConnection">
+                  <h2 className="numero">03</h2>
+                  <h3 className="titre">Sécurité</h3>
+                  <p className="paragrapheConnection">Petite description...</p>
+                  <Link to={"/LoginSecurite"}  className="btnConnection">Entrer</Link>
               </div>
             </div>
         </div>
